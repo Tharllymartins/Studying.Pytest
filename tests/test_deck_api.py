7e91f3735f0cdf29c8b08083b87ef0fcc7ef63b2
Tharlly Alves Martins <tharllymartins16@gmail.com>
2022-08-08 18:27:44 -0300

    added print to name a test when run in a prompt

--- tests/test_deck_api.py
+++ tests/test_deck_api.py
@@ -25,6 +25,8 @@ def test_draw_a_cart_from_a_deck(deck_data):
     
     status_code_is_ok = response.status_code in [200, 201]
     
+    print("\nDraw a cart from the deck")
+    
     assert status_code_is_ok
     if status_code_is_ok: print("\nStatus code is ok")
     assert remaining_cards_has_decreased
@@ -37,6 +39,8 @@ def test_reshuffle_cards_from_a_deck(deck_data):
     response = requests.post(f"http://deckofcardsapi.com/api/deck/{deck_id}/shuffle/")
     
     status_code_is_ok = response.status_code in [200, 201]
+    
+    print("Reshuffle cards from the deck")
     
     if status_code_is_ok: print("\nStatus code is ok")
     assert status_code_is_ok
@@ -52,6 +56,8 @@ def test_get_a_brand_new_deck(deck_data):
     new_deck_created = old_deck_id != new_deck_id
     
     status_code_is_ok = response.status_code in [200, 201]
+    
+    print("Get a brand new deck")
     
     if new_deck_created: print("\nNew deck created")
     assert new_deck_created
